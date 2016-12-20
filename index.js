b98c8af122360cf1ec6ef78b40532a732a085d65
Yechiel K <chilikkal@gmail.com>
2016-12-19 21:32:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+Handlebars.registerHelper('comment_body', function () {
+  if (this.state === 'closed') {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
 
 function loadIssues() {
 
