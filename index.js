43dee9c29fac529cf357ec7a335bc39a7ab3069b
Yechiel K <chilikkal@gmail.com>
2016-12-19 21:01:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+function loadIssues() {
+
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
