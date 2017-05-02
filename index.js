d07dd4dc23f79e0ca1332ac442f8ef302f91cdd9
Alex Glover <Alex@MacBook-Pro-3.local>
2017-05-02 18:24:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+Handlebars.registerHelper('commentBody', function() {
+  if (this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
 
+function loadIssuse() {
+
+  var template = Handlebars.compile(document.getElementById('issue-template').innerHTML);
+  var result = template(issues);
+
+  document.getElementsByTagName('main')[0].innerHTML += result
+}
