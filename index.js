5ee08fd4d59014461d9b77a9a702e35b6c66b039
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-05-10 11:59:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+function loadIssue() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  for(var i=0;i<issues.length;i++) {
+    var result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
+}
