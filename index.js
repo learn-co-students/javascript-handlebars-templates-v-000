9323e9f5b4b675011833917c3fde3afd232ef840
Mollie <mollie.stein@gmail.com>
2017-09-03 16:40:54 -0400

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ function loadIssue() {
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
 
-function loadIssue() {
+function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
