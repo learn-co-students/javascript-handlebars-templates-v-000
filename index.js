531c1b5e64c292915bb3506489c8f201b2c554b2
Mollie <mollie.stein@gmail.com>
2017-09-03 16:39:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function loadIssue() {
+  var issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  }
+
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+function loadIssue() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  for(var i=0;i<issues.length;i++) {
+    var result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
+}
