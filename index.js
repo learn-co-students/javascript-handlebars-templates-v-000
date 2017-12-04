542861619af7c04254753216c3a644f6bb1cfbfa
Alexandra Wright <superbiscuit@gmail.com>
2017-12-04 08:42:07 -0800

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-
-function loadIssues() {
-    var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-    var result = template(issues);
-    document.getElementsByTagName("main")[0].innerHTML += result;
+function init() {
+    //put any page initialization/handlebars initialization here
+    loadRecipeTemplate()
+    Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+    Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-template").innerHTML)
+    Handlebars.registerHelper('displayIngredient', function () {
+        return new Handlebars.SafeString("<li class='newIngredient'>" + this + "</li>")
+    })
 }
 
-Handlebars.registerHelper('comment_body', function () {
-    if (this.state === "closed") {
-        return new Handlebars.SafeString(this.body)
-    } else {
-        return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
-    }
+document.addEventListener("DOMContentLoaded", function (event) {
+    init()
 })
 
-function loadIssues() {
-    //...
+function createRecipe() {
+    const name = document.getElementById("name").value
+    const description = document.getElementById("recipeDescription").value
+    const nodeIngredients = document.getElementsByClassName("recipeIngredient")
+    let ingredients = []
+    for (const node of nodeIngredients) {
+        ingredients.push(node.value)
+    }
+
+    let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+    let results = template({ "name": name, "description": description, "ingredients": ingredients })
+    document.getElementsByTagName("main")[0].innerHTML += results
+}
+
+function loadRecipeTemplate() {
+
+    let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+    document.getElementsByTagName("main")[0].innerHTML += template()
+
+}
+
+function displayEditForm() {
+
+    let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+    document.getElementsByTagName("main")[0].innerHTML += template()
+
+}
+
+function updateRecipe() {
+    createRecipe()
 }
