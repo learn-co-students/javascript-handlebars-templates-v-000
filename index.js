db4b32c137876e38ed91daa65ab94a62e12e9a96
Tina Hang <mstinahang@gmail.com>
2018-02-07 05:42:54 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Handlebars.registerHelper('comment_body', function() {
   }
 })
 
+
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   var result = template(issues);
